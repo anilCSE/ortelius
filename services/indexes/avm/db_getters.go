098d942ae55ae6c14ddcd1c206fe6caff042a3b6
Todd Stock <todd@avalabs.org>
2020-09-13 08:49:15 -0400

    refactor logic

--- services/indexes/avm/db_getters.go
+++ services/indexes/avm/db_getters.go
@@ -251,6 +251,9 @@ func (db *DB) ListTransactions(ctx context.Context, p *ListTransactionsParams) (
 		Distinct().
 		From("avm_transactions").
 		Where("avm_transactions.chain_id = ?", db.chainID))
+	if p.NeedsDistinct() {
+		builder = builder.Distinct()
+	}
 
 	var applySort func(sort TransactionSort)
 	applySort = func(sort TransactionSort) {
@@ -277,9 +280,17 @@ func (db *DB) ListTransactions(ctx context.Context, p *ListTransactionsParams) (
 	count := uint64(p.Offset) + uint64(len(txs))
 	if len(txs) >= p.Limit {
 		p.ListParams = params.ListParams{}
-		err := p.Apply(dbRunner.
-			Select("COUNT(avm_transactions.id)").
-			From("avm_transactions")).
+		var selector *dbr.SelectStmt
+		if p.NeedsDistinct() {
+			selector = p.Apply(dbRunner.
+				Select("COUNT(DISTINCT(avm_transactions.id))").
+				From("avm_transactions"))
+		} else {
+			selector = p.Apply(dbRunner.
+				Select("COUNT(avm_transactions.id)").
+				From("avm_transactions"))
+		}
+		err := selector.
 			LoadOneContext(ctx, &count)
 		if err != nil {
 			return nil, err
